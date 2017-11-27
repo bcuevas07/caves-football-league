a33b12b1cf58ef576f6cf8e22607d5c4db7d596f
bcuevas07 <bcuevas07@yahoo.com>
2017-11-26 20:42:54 -0800

    Correcting path to the base settings file as defined in manage.py

--- nfl_app/manage.py
+++ nfl_app/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nfl_app.settings")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nfl_app.config.settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "   config.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
